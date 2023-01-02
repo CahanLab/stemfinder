c9d6d48b23f1a849589ce3065a35836da76fec52
kma2112 <kma2112@gmail.com>
2023-01-01 21:15:47 -0500

    removed first stemFinder version & old readme

--- stemFinder/R/gene_set_score.R
+++
@@ -1,9 +0,0 @@
-
-gene_set_score <- function(gene.set, adata){
-  
-  gene.set = gene.set[gene.set %in% rownames(adata)]
-  mean.exp <- colMeans(x = adata@assays$RNA@data[gene.set, ], na.rm = TRUE)
-  adata@meta.data$gene.set.score <- mean.exp
-  
-  return(adata)
-}
